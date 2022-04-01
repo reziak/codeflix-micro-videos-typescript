e5be2e38fe0befde94b222b358295b6d852c49e6
Bruno Lira <bruno.padovan@gmail.com>
2022-04-01 20:19:58 -0300

    Category unit tests

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
 export class Category {
-  constructor(public name : string) {}
+  public readonly id: string;
+
+  constructor(public readonly props: CategoryProperties, id?: string) {
+    this.id = id || uuidv4();
+    this.description = this.props.description;
+    this.is_active = this.props.is_active;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value ?? true;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
